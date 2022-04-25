3deae038c78940616f03268832e718a2597c3b07
Gabriell-Murta <gabriellmurta@icloud.com>
2022-04-25 14:36:38 -0300

    EntityMapper Adjustment

--- data/src/main/java/com/example/data/company/mapper/CompanyEntityMapper.java
+++ data/src/main/java/com/example/data/company/mapper/CompanyEntityMapper.java
@@ -2,11 +2,13 @@ package com.example.data.company.mapper;
 
 import com.example.core.company.company.Company;
 import com.example.data.company.entity.CompanyEntity;
+import com.example.data.product.mapper.ProductEntityMapper;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValueMappingStrategy;
 import org.springframework.data.jpa.repository.JpaContext;
 
-@Mapper(componentModel = "spring")
+@Mapper(uses = ProductEntityMapper.class, nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface CompanyEntityMapper {
 
   CompanyEntity toEntity(Company companyEntity, @Context JpaContext ctx);
--- data/src/main/java/com/example/data/product/mapper/ProductEntityMapper.java
+++ data/src/main/java/com/example/data/product/mapper/ProductEntityMapper.java
@@ -1,13 +1,15 @@
 package com.example.data.product.mapper;
 
 import com.example.core.product.product.Product;
+import com.example.data.member.mapper.MemberEntityMapper;
 import com.example.data.product.entity.ProductEntity;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
 import org.springframework.data.jpa.repository.JpaContext;
 
-@Mapper(componentModel = "spring")
+@Mapper(uses = MemberEntityMapper.class, nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public interface ProductEntityMapper {
 
   ProductEntity toEntity(Product productEntity, @Context JpaContext ctx);
