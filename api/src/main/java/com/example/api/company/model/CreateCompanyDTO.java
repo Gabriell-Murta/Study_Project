1f4e4ae760796c2324d43232d03a314647a31687
Gabriell-Murta <gabriellmurta@icloud.com>
2022-05-06 10:19:26 -0300

    Request Exceptions Adjust

--- api/src/main/java/com/example/api/company/model/CreateCompanyDTO.java
+++ api/src/main/java/com/example/api/company/model/CreateCompanyDTO.java
@@ -1,14 +1,14 @@
 package com.example.api.company.model;
 
 import com.example.api.product.model.CreateProductDTO;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateCompanyDTO {
 
-  @NotBlank
-  private String name;
-  private CreateProductDTO product;
+  @NotBlank private String name;
+  @Valid private CreateProductDTO product;
 
 }
